fbd1febc9f2b90115e4e4ff55f1301c58cb9eddc
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-10-16 14:50:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>IMDB of Games</h1>
+      <h2>It is a complete all in one location for games information. It will provide information related to games like rating, storyline,
+        System requirements , platform on which it is available etc.<br/>
+         At the top it'll contain a genre menu in which all type of genres are available like Action, Sports, Adeventure, Fantasy etc.<br/>
+        User can select the type of game it want by direct search or by tags of genres.<br/>
+        There will also be an option for user to give hiss feedback if he doesn't find the information given useful.
+
+         </h2>
+    </div>
+  );
+}
